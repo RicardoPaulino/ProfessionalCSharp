fedab6cbcca611a0a78586e2436e97ed37b4a679
ricardo.paulino.nascimento <ricardo.paulino.nascimento@hotmail.com>
2022-12-19 23:31:23 -0300

    format numerical data

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -14,7 +14,22 @@ namespace Chapter2
             Console.WriteLine("***************************************************");
             Console.BackgroundColor = ConsoleColor.Black;
 
+            FormatNumericalData();
+
             Console.ReadLine();
+        }
+
+        private static void FormatNumericalData()
+        {
+            Console.WriteLine("The value 99999 in various formats");
+            Console.WriteLine("C format: {0:c}",99999);
+            Console.WriteLine("d9 format: {0:d9}", 99999);
+            Console.WriteLine("f3 format: {0:f3}", 99999);
+            Console.WriteLine("n format: {0:n}", 99999);
+            Console.WriteLine("E format: {0:E}", 99999);
+            Console.WriteLine("e format: {0:e}", 99999);
+            Console.WriteLine("X format: {0:X}", 99999);
+            Console.WriteLine("x format: {0:x}", 99999);
         }
     }
 }
